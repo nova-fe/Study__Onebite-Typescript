82a99e36394e3d8cadb56bef72cb59fe84fae53e
Nova <heetask@gmail.com>
2025-07-01 14:20:31 +0900

    자바스크립트의 클래스 소개 - 완료

--- section06/src/chapter0.js
+++ section06/src/chapter0.js
@@ -32,12 +32,32 @@ class Student {
     console.log("공부를 합니다.");
   }
   introduce() {
-    console.log("안녕하세요");
+    console.log(`안녕하세요, ${this.name} 입니다.`);
   }
 }
 
+class StudentDeveloper extends Student {
+  // 필드
+  favoriteSkill;
+
+  // 생성자
+  constructor(name, grage, age, favoriteSkill) {
+    super(name, grage, age); // 부모 클래스의 생성자를 호출
+    this.favoriteSkill = favoriteSkill;
+  }
+
+  // 메서드
+  programming() {
+    console.log(`${this.programming}으로 프로그래밍을 합니다.`);
+  }
+}
+
+const studentDeveloper = new StudentDeveloper("Nova2", "A+", 33, "Typscript");
+
 // 클래스를 이용해서 만든 객체 -> 인스턴스
 // 스튜던트 인스턴스
-let studentB = new Student("KNT", "B", 20);
-console.log(studentB); // Student { name: 'KNT', grage: 'B', age: 20 }
-studentB.study(); // 공부를 합니다.
+let studentB = new Student("Nova", "B", 20);
+
+console.log(studentDeveloper);
+studentDeveloper.study();
+studentDeveloper.introduce();
